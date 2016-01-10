a48e312a15ba9f609d02376706f60ef902e8363b
Matthew Mettler <matthewrmettler@gmail.com>
2016-01-10 16:24:49 -0500

    Initial commit. Using basis from project that I've already started.

---
+++ index.js
@@ -0,0 +1,65 @@
+/******************
+ * Initialization *
+ ******************/
+
+//Import from modules and libraries
+var express = require('express');
+
+//Initialize app
+var app = express();
+
+//Set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//Set the port
+app.set('port', process.env.PORT || 3000);
+
+//Set up static middleware
+app.use(express.static(__dirname + '/public'));
+
+
+/******************
+ * Routing Table  *
+ ******************/
+
+//Testing
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+//Main
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+//About
+app.get('/about', function(req, res) {
+	res.render('about', { 
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+
+//Custom 404
+app.use( function(req, res) {
+	res.status(404);
+	res.render('404');
+});
+
+//Custom 500
+app.use( function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+/******************
+ *     Start      *
+ ******************/
+
+//Start the server
+app.listen(app.get('port'), function() {
+	console.log('Express started on localhost:' + app.get('port'));
+})
