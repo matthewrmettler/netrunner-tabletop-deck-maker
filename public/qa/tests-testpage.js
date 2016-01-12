306dd80939e579b090d07f74b0431ad5a11cf00d
Solarswordsman <jeffarsu@gmail.com>
2016-01-12 15:21:58 -0500

    Didn't fix anything

--- public/qa/tests-testpage.js
+++ public/qa/tests-testpage.js
@@ -1,5 +1,5 @@
 suite('Testpage Tests', function() {
 	test('Uploading file to imgur works', function() {
-		assert(uploadtoImgur("/img/cards/00005.png"));
+		assert(uploadToImgur("/img/cards/00005.png"));
 	});
 });
